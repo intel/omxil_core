09b55fbace682dc3315114e81a1d2c6e02a5718c
Ho-Eun Ryu <ho-eun.ryu@windriver.com>
2009-09-25 14:30:46 +0900

    utils:thread: fix bug with thread id handling

--- utils/src/thread.cpp
+++ utils/src/thread.cpp
@@ -4,30 +4,21 @@
 Thread::Thread()
 {
     r = NULL;
-    id = -1;
 }
 
 Thread::Thread(RunnableInterface *r)
 {
     this->r = r;
-    id = -1;
 }
 
 Thread::~Thread()
 {
-    if (id > 0)
-        Join();
+    Join();
 }
 
 int Thread::Start(void)
 {
-    int ret;
-
-    ret = pthread_create(&id, NULL, Instance, this);
-    if (ret)
-        id = -1;
-
-    return ret;
+    return pthread_create(&id, NULL, Instance, this);
 }
 
 int Thread::Join(void)
