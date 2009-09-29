6417dc5c0f9638516ec6770179280f7723c47090
Ho-Eun Ryu <ho-eun.ryu@windriver.com>
2009-09-29 17:14:50 +0900

    base:cmodule: change name -> lname (library name)

--- base/inc/cmodule.h
+++ base/inc/cmodule.h
@@ -14,14 +14,14 @@ typedef OMX_ERRORTYPE (*cmodule_exit_t)(CModule *);
 
 class CModule {
  public:
-    CModule(const OMX_STRING name);
+    CModule(const OMX_STRING lname);
     ~CModule();
 
     OMX_ERRORTYPE Load(void);
     OMX_ERRORTYPE Unload(void);
 
-    void SetName(const OMX_STRING name);
-    const OMX_STRING GetName(void);
+    const OMX_STRING GetLibraryName(void);
+
     void SetPrivData(OMX_PTR privdata);
     OMX_PTR GetPrivData(void);
 
@@ -29,7 +29,7 @@ class CModule {
     cmodule_init_t init;
     cmodule_exit_t exit;
 
-    char name[OMX_MAX_STRINGNAME_SIZE];
+    char lname[OMX_MAX_STRINGNAME_SIZE];
     struct module *module;
 
     OMX_PTR privdata;
--- base/src/cmodule.cpp
+++ base/src/cmodule.cpp
@@ -13,17 +13,17 @@
 #define LOG_TAG "cmodule"
 #include <log.h>
 
-CModule::CModule(const OMX_STRING name)
+CModule::CModule(const OMX_STRING lname)
 {
-    memset(name, 0, OMX_MAX_STRINGNAME_SIZE);
+    memset(lname, 0, OMX_MAX_STRINGNAME_SIZE);
 
     module = NULL;
     privdata = NULL;
     init = NULL;
     exit = NULL;
 
-    strncpy(this->name, name, OMX_MAX_STRINGNAME_SIZE);
-    name[OMX_MAX_STRINGNAME_SIZE-1] = '\0';
+    strncpy(this->lname, lname, OMX_MAX_STRINGNAME_SIZE);
+    lname[OMX_MAX_STRINGNAME_SIZE-1] = '\0';
 }
 
 CModule::~CModule()
@@ -38,23 +38,23 @@ OMX_ERRORTYPE CModule::Load()
     if (module)
         return OMX_ErrorNotReady;
 
-    module = module_open(name, MODULE_NOW);
+    module = module_open(lname, MODULE_NOW);
     if (!module) {
-        LOGE("module not founded (%s)\n", name);
+        LOGE("module not founded (%s)\n", lname);
         return OMX_ErrorComponentNotFound;
     }
-    LOGV("module founded (%s)\n", name);
+    LOGV("module founded (%s)\n", lname);
 
     init = (cmodule_init_t)module_symbol(module, "omx_component_module_init");
     if (!init) {
-        LOGE("module init symbol not founded (%s)\n", name);
+        LOGE("module init symbol not founded (%s)\n", lname);
         module_close(module);
         return OMX_ErrorInvalidComponent;
     }
 
     exit = (cmodule_exit_t)module_symbol(module, "omx_component_module_exit");
     if (!exit) {
-        LOGE("module exit symbol not founded (%s)\n", name);
+        LOGE("module exit symbol not founded (%s)\n", lname);
         module_close(module);
         return OMX_ErrorInvalidComponent;
     }
@@ -67,7 +67,7 @@ OMX_ERRORTYPE CModule::Load()
         return init_ret;
     }
 
-    LOGV("module %s successfully loaded\n", name);
+    LOGV("module %s successfully loaded\n", lname);
     return OMX_ErrorNone;
 }
 
@@ -87,13 +87,13 @@ OMX_ERRORTYPE CModule::Unload(void)
     module_close(module);
     module = NULL;
 
-    LOGV("module %s successfully unloaded\n", name);
+    LOGV("module %s successfully unloaded\n", lname);
     return OMX_ErrorNone;
 }
 
-const OMX_STRING CModule::GetName(void)
+const OMX_STRING CModule::GetLibraryName(void)
 {
-    return name;
+    return lname;
 }
 
 void CModule::SetPrivData(OMX_PTR privdata)
