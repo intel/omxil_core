35f454e3517f2e5e76dbd5d76a3baa66a501b36f
Ho-Eun Ryu <ho-eun.ryu@windriver.com>
2009-10-12 18:11:08 +0900

    base:port: unused variable(state)

--- base/src/portbase.cpp
+++ base/src/portbase.cpp
@@ -267,7 +267,6 @@ OMX_ERRORTYPE PortBase::UseBuffer(OMX_BUFFERHEADERTYPE **ppBufferHdr,
 {
     OMX_BUFFERHEADERTYPE *buffer_hdr;
     struct list *entry;
-    OMX_STATETYPE state;
 
     pthread_mutex_lock(&hdrs_lock);
 
@@ -328,7 +327,6 @@ OMX_ERRORTYPE PortBase::AllocateBuffer(OMX_BUFFERHEADERTYPE **ppBuffer,
 {
     OMX_BUFFERHEADERTYPE *buffer_hdr;
     struct list *entry;
-    OMX_STATETYPE state;
 
     pthread_mutex_lock(&hdrs_lock);
     if (portparam.bPopulated == OMX_TRUE) {
@@ -386,7 +384,6 @@ OMX_ERRORTYPE PortBase::FreeBuffer(OMX_U32 nPortIndex,
                                    OMX_BUFFERHEADERTYPE *pBuffer)
 {
     struct list *entry;
-    OMX_STATETYPE state;
     OMX_ERRORTYPE ret;
 
     pthread_mutex_lock(&hdrs_lock);
