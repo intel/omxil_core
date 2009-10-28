11ff4738104392273619125a254993c03c4c1db0
Ho-Eun Ryu <ho-eun.ryu@windriver.com>
2009-10-28 19:39:49 +0900

    misc: add windriver standard copyright

--- base/inc/portimage.h
+++ base/inc/portimage.h
@@ -1,6 +1,9 @@
 /*
- * Copyright (C) 2009 Wind River Systems
- *      Author: Ho-Eun Ryu <ho-eun.ryu@windriver.com>
+ * Copyright (c) 2009 Wind River Systems, Inc.
+ *
+ * The right to copy, distribute, modify, or otherwise make use
+ * of this software may be licensed only pursuant to the terms
+ * of an applicable Wind River license agreement.
  */
 
 #ifndef __PORTIMAGE_H
